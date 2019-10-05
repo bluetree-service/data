eadd1e735c6a7b45b9ced11aac7d1da74954da7a
chajr <chajr@bluetree.pl>
2019-10-05 13:44:48 +0200

    Added empty class for future usage

---
+++ src/Data/SimpleXml.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @author Michał Adamiak <michal.adamiak@lizardmedia.pl>
+ * @copyright Copyright (C) 2019 Lizard Media (http://lizardmedia.pl)
+ */
+
+namespace BlueData\Data;
+
+class SimpleXml
+{
+    public function __construct($xml)
+    {
+        
+    }
+
+    public function arrayToXml()
+    {
+        
+    }
+
+    public function xmlToArray()
+    {
+        
+    }
+}
